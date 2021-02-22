e0e066abaa2b37b07d15337a83f2e66639d4f51a
Administrator <admin@example.com>
2021-02-22 09:18:32 +0000

    Update db/mariadb-login.sql

--- db/mariadb-login.sql
+++ db/mariadb-login.sql
@@ -5,7 +5,6 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE DATABASE `login` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `login`;
 
 SET NAMES utf8mb4;
